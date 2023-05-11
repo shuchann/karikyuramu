2801b6f88faf40158edd21c1723c945f16e095a0
shuchann <g021c1023@g.neec.ac.jp>
2023-05-11 14:32:16 +0900

    8-2

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
+    }
+}
